99f1f32b0090ce4c3bfc3270c17eeee435aae7d4
Berto <albertoraffa2006@gmail.com>
2026-03-12 20:11:22 +0100

    Adjusted projectiles and health bar

--- scripts/enemy FSM/attackState_enemy.gd
+++ scripts/enemy FSM/attackState_enemy.gd
@@ -2,31 +2,34 @@ extends EnemyState
 
 @onready var attack_timer : Timer = $AttackTimer
 @onready var projectile = preload("res://scenes/logics/projectile-test.tscn")
-	
+@export var hitbox_shape : Shape2D	
 
 func enter() -> void:
-	attack_timer.start()
-	enemy.velocity = Vector2.ZERO
+    enemy = owner as Enemy
+    attack_timer.start()
+    enemy.velocity = Vector2.ZERO
 
 func update(_delta : float) -> void:
-	if not enemy.is_player_near():
-		attack_timer.stop()
-		enemy.state_machine.change_state("MovingRandomState")
+    if not enemy.is_player_near():
+        attack_timer.stop()
+        enemy.state_machine.change_state("MovingRandomState")
 
 func shoot() -> void:
-	var bullet = projectile.instantiate()
-	bullet.direction = (enemy.player.position - enemy.muzzle.position).angle()
-	bullet.spawnPos = enemy.muzzle.position
-	bullet.spawnRot = enemy.global_rotation
-	enemy.get_parent().add_child(bullet)
+    var bullet = projectile.instantiate()
+    bullet.direction = (enemy.player.position - enemy.muzzle.position).angle()
+    bullet.spawnPos = enemy.muzzle.position
+    bullet.spawnRot = enemy.global_rotation
+    enemy.get_parent().add_child(bullet)
+    var hitbox = Hitbox.new(enemy.stats, 0.5, hitbox_shape, enemy)
+    bullet.add_child(hitbox)
 
 func multipleShoot() -> void: #this doesn't work :(
-	for n in range(1,4):
-		var bullet = projectile.instantiate()
-		bullet.spawnPos = enemy.muzzle.position + Vector2(n * 10, n * 5)
-		bullet.spawnRot = enemy.global_rotation
-		bullet.direction = (enemy.player.position - (enemy.muzzle.position + Vector2(n * 10, n * 5))).angle()
-		enemy.get_parent().add_child(bullet)
+    for n in range(1,4):
+        var bullet = projectile.instantiate()
+        bullet.spawnPos = enemy.muzzle.position + Vector2(n * 10, n * 5)
+        bullet.spawnRot = enemy.global_rotation
+        bullet.direction = (enemy.player.position - (enemy.muzzle.position + Vector2(n * 10, n * 5))).angle()
+        enemy.get_parent().add_child(bullet)
 
 func _on_attack_timer_timeout() -> void:
-	shoot()
+    shoot()
--- scripts/logics/projectile_test.gd
+++ scripts/logics/projectile_test.gd
@@ -1,28 +1,28 @@
 extends CharacterBody2D
 
 @export var speed : int = 200
-
 var direction : float
 var spawnPos : Vector2
 var spawnRot : float
 
+@onready var shape = $CollisionShape2D
+
 func _ready():
-    global_position = spawnPos
-    global_rotation = spawnRot
+	global_position = spawnPos
+	global_rotation = spawnRot
 
 func _physics_process(delta: float) -> void:
-    velocity = Vector2(speed, get_gravity().y*delta).rotated(direction)
-    move_and_slide()
+	velocity = Vector2(speed, get_gravity().y*delta).rotated(direction)
+	move_and_slide()
+
+
+func _on_area_2d_area_entered(area: Area2D) -> void:
+	if area.find_parent("FlyEnemy"):
+		return
+	queue_free()
 
 
 func _on_area_2d_body_entered(body: Node2D) -> void:
-    if body == self or body.find_parent("FlyEnemy"):
-        return
-    if body is Player:
-        body.stats.take_damage(30) #yes, I know it sucks. Gotta fix it
-    queue_free()
-
-func _on_area_2d_area_entered(area: Area2D) -> void:
-    if area.find_parent("FlyEnemy"): 
-       return 
-    queue_free()
+	if not body is FlyEnemy or body != self:
+		print(body)
+		queue_free()
